52f8f6e0e43e2382c5b8a5f187bef98f403f9c22
Samuel Medeiros <42820519+SamuelMMedeiros@users.noreply.github.com>
2025-10-15 18:35:45 -0300

    Update headings in Home component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -73,9 +73,9 @@ const Home = () => {
                 <div className="absolute inset-0 bg-gradient-hero" />
                 <div className="relative z-10 container mx-auto px-4 sm:px-6 animate-fade-in">
                     <h1 className="text-heading-xl md:text-6xl lg:text-7xl text-white font-bold mb-6">
-                        Conheça os Animais
+                        Conheça os Morcegogs
                         <br />
-                        Peçonhentos do Brasil
+                        Mais Comuns de Patos de Minas
                     </h1>
                     <p className="text-body-large md:text-xl text-white/90 mb-8 max-w-2xl mx-auto">
                         Informações científicas confiáveis para promover a
