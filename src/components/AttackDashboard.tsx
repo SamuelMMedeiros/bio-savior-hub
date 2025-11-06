226f950fd109c8e1098aaffe9cf6955aaf3adeb3
Samuel Medeiros <samuelmmedeiros20@gmail.com>
2025-11-06 00:34:52 -0300

    Correções realizadas no gráfico e no mapa

--- src/components/AttackDashboard.tsx
+++ src/components/AttackDashboard.tsx
@@ -15,15 +15,14 @@ export function AttackDashboard() {
     return (
         /*md:flex-row - exibe o mapa e o gráfico lado a lado em telas grandes */
         <div className="flex flex-col gap-6 w-full">
-            <div className="flex-1 h-[400px] md:h-[500px] rounded-lg overflow-hidden shadow">
-                <AttackMap center={selectedBairro} />
-            </div>
-
-            <div className="flex-1">
+            <div className="min-h-[400px]">
                 <AttackChart
                     data={ATTACK_DATA}
                     onBairroClick={handleBairroClick}
                 />
+            </div>
+            <div className="h-[400px] md:h-[500px] rounded-lg overflow-hidden shadow">
+                <AttackMap center={selectedBairro} />
             </div>
         </div>
     );
