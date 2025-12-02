3c1d7c4815b2f649daed50e3c30dff6b15f8e7d0
Samuel Medeiros <42820519+SamuelMMedeiros@users.noreply.github.com>
2025-12-01 23:17:08 -0300

    Atualizar o About.tsx

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -171,7 +171,7 @@ const About = () => {
                     </p>
 
                     <a
-                        href="/downloads/BioStats_TECNOLOGIA_E_EDUCAÇÃO_NA_PREVENÇÃO_DE_ACIDENTES_COM_MORCEGOS_EM_ÁREAS_URBANAS.pdf"
+                        href="/download/BioStats_TECNOLOGIA_E_EDUCAÇÃO_NA_PREVENÇÃO_DE_ACIDENTES_COM_MORCEGOS_EM_ÁREAS_URBANAS.pdf"
                         download
                         className="inline-flex items-center gap-2 bg-primary text-primary-foreground px-8 py-3 rounded-2xl font-medium shadow-md hover:bg-primary/90 transition-colors"
                     >
